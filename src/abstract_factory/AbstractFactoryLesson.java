2145aed919c8efdc8268ba5b075d016b6015a29d
Dan Serbyn <denis.serbyn@gmail.com>
2018-10-20 22:49:41 +0300

    Some additions to Abstract Factory pattern's description

--- src/abstract_factory/AbstractFactoryLesson.java
+++ src/abstract_factory/AbstractFactoryLesson.java
@@ -6,6 +6,7 @@ import java.util.List;
 public class AbstractFactoryLesson {
 
     /* Pattern: Abstract Factory
+     * (Creational pattern)
      * ********************
      * DESCRIPTION
      * ********************
