ccaa2c2fd1d09305c6c061a77c2d1b0fc47a5b83
Dan Serbyn <denis.serbyn@gmail.com>
2018-10-22 10:16:47 +0300

    Started to work with Factory Method pattern

---
+++ src/factory_method/FactoryMethodLesson.java
@@ -0,0 +1,20 @@
+package factory_method;
+
+public class FactoryMethodLesson {
+
+    /* Pattern: Factory Method
+     * (Creational pattern)
+     * ********************
+     * DESCRIPTION
+     * ********************
+     * EN:
+     * //english description here
+     * RU:
+     * //english description here
+     *
+     */
+
+    public void main(){
+        //launch code here
+    }
+}
